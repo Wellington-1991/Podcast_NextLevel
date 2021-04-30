589a50713de4d613d56c6648cb8aa84c46f5ab20
Wellington-1991 <well_rafa@live.com>
2021-04-29 21:11:20 -0300

    importando o next para o inertex.cs

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -2,8 +2,7 @@ import { useRouter } from 'next/router'
 
 export default function Episode() {
     const router = useRouter();
-
     return (
-        <h1>router.query.slug</h1>
+        <h1>{router.query.slug}</h1>
     )
 }
